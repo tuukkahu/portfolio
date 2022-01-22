c3fc0868025066de56a292efd005382a080eaef6
tuukkahu <62805986+tuukkahu@users.noreply.github.com>
2022-01-22 16:54:44 +0200

    Edit style

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,8 +2,9 @@ const Tile = ({ info }) => {
     let classname=`tile ${info.classnames}`
     return (
         <article className={classname}>
-            <h4 className="">{info.title}</h4>
-            <p>{info.description}</p>
+            <h4 className="card-title">{info.title}</h4>
+            <p className="card-body">{info.description}</p>
+            <a href="/" className="btn">Flip card</a>
         </article>
     )
 }
